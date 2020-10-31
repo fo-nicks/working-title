d01960cb33b7f4b498cba3b648c970f6b3460446
jachrispens <jason@chrispens.org>
2020-10-31 15:58:53 -0700

    Added rough design notes for a peer protocol. (#9)
    The start of a 'peer' module where the definition of a Peer for this application might live... I added the start of some design notes about a protocol that might be used.
    
    A couple of thoughts that aren't in the those notes. An interesting aspect of this kind of service is that there's really hard guarantees about when anything needs to happen, as long as it does with a reasonable time frame, and reasonable in this case my be measured in hours, maybe even days. That may open up some interesting possibilities for protocol design.

---
+++ src/peer.rs
@@ -0,0 +1,33 @@
+/// The peer module defines traits and structures relevant to Peers.
+/// As the name suggegts, Peers are equal participants in the application -
+/// any peer can make a direct request of any other peer.
+///
+/// Peers communicate with each other over some bidirectional communication 
+/// channel.  The exact form of this channel is left abstract, but possible
+/// examples are TCP, Secure Sockets, SSH, HTTP/S, etc.  Whatever the channel,
+/// it is presumed to be generally reliable, though Peers are not expected to
+/// be always present.  
+/// 
+/// In the current design, Peers communicate using a mostly stateless protocol.  
+/// At the base level of the protocol are 'commands'.  Commands are a way of
+/// passing (structured) data in a single direction.  The underlying transport 
+/// is expected to reliably deliver data.
+/// 
+/// Commands consist of an opcode, indicating which command is to be executed,
+/// a command ID, which will be used to identify the command over a certain
+/// bound, a time to live (TTL) and the arguments (or data) specific to that 
+/// command.  The identifier is also used to tie requests and responses together.  
+/// The core idea is that any peer is able to send any command at any time to 
+/// any other peer.  Thus, at its core, the protocol is not stateful and asyncronous.
+/// The commands themselves are design to account for out-of-order execution and
+/// to be idempotent.
+/// 
+/// Within the commands themselves is the ability to track outstanding commands etc.
+/// this is expanded below, in the explanation for the commands.
+/// 
+/// Two aspects that this protocol does not address is security/encryption and authenciation.
+/// It is assumed that the lower level transport mechanism will do this.  That is, part of
+/// establishing a bi-directional channel will be to authenticate and ensure that security
+/// needs are met.  This protocol will use a so-established channel.  (Note that this 
+/// does not mean that user data communicated by this protocol won't be encrypted, it means
+/// that the parts of this protocol, op-codes, command IDs, etc. won't be.)
